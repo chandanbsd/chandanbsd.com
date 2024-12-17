40deac4ecbf3e6189bb795f31251e66a5316c5a8
chandanbsd <49138440+chandanbsd@users.noreply.github.com>
2024-12-16 21:45:32 -0500

    Updates

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Toolbar } from 'primeng/toolbar';
+import { AvatarModule } from 'primeng/avatar';
+import { SharedModule } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss',
+  standalone: true,
+  imports: [Toolbar, AvatarModule, ButtonModule, SharedModule]
+})
+export class NavbarComponent {
+
+}
